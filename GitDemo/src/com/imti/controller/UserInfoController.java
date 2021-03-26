5e2045cfdd0f2241778455079ff3fcabf2f60d4e
guoHaiFeng <1191619897@qq.com>
2021-03-26 14:49:26 +0800

    郭海锋提交UserInfoController

---
+++ GitDemo/src/com/imti/controller/UserInfoController.java
@@ -0,0 +1,19 @@
+package com.imti.controller;
+/**@文件名: UserInfoController.java
+ * @类功能说明: 
+ * @作者: GuoHaiFeng
+ * @Email: 1191619897@qq.com
+ * @日期: 2021年3月26日下午2:48:01
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: GuoHaiFeng</li> 
+ * 	 <li>日期: 2021年3月26日下午2:48:01</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class UserInfoController {
+
+	public static void main(String[] args) {
+		System.out.println("git演示");
+	}
+}
